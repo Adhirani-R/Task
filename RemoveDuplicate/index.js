79b9d9324cdb1e37cd6a23664fa41cefa22571f2
Adhirani-R <154663878+Adhirani-R@users.noreply.github.com>
2024-01-26 21:04:21 +0530

    Update index.js

--- RemoveDuplicate/index.js
+++ RemoveDuplicate/index.js
@@ -1,15 +1,4 @@
-/* var mySet = new Set();
-mySet.add(1);
-mySet.add(2);
-mySet.add(3);
-mySet.add(4);
-mySet.add(1);
-mySet.add(5);
-mySet.add(1);
-mySet.add(6);
-mySet.add(3);
-mySet.add(7); 
-console.log(mySet); */ //ignores the duplicate value
+
 
 var inputArray = [1,2,1,3,4,2,5,6,1,7]
   var uniqueValue = Array.from(new Set(inputArray))
